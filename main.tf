cc9ec27d33192c79d5c9720f8ecc613f12c2a376
owwe <berkan-yapici@hotmail.com>
2024-01-09 01:48:06 +0100

    lambda_function last_version

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "data-ingestor-lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   image_uri      = "477472432946.dkr.ecr.eu-north-1.amazonaws.com/daily-data-ingestor-lambda-image:latest"
-  function_name = "btc_faeture_store_data_ingestor"
+  function_name = "btc_feature_store_data_ingestor"
   role          = aws_iam_role.iam_for_data_ingestor_lambda.arn
   package_type  = "Image"
   #source_code_hash = data.archive_file.lambda.output_base64sha256
@@ -50,7 +50,7 @@ resource "aws_scheduler_schedule" "btc-data-ingestor-sch" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(0 0/4 * * ? 2024)"  # Run every 4 hours starting from 2024-01-09 00:00
+  schedule_expression = "cron(0 0/2 * * ? 2024)"  # Run every 2 hours starting from 2024-01-09 00:00
 
   schedule_expression_timezone = "UTC"
   start_date = "2024-01-09T01:55:55Z"
