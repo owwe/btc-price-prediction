fa97a234bae31c1cce8db235a51868a7712ee2cc
owwe <berkan-yapici@hotmail.com>
2024-01-11 02:46:31 +0100

    last_version

--- lambda_btc_price_ingestor/src/main.py
+++ lambda_btc_price_ingestor/src/main.py
@@ -25,26 +25,4 @@ def handler(event,context):
     #feature_group.load_feature_definitions(df)
     feature_group.ingest(data_frame=df, wait=True)
 
-    if datetime.now().hour == 0 or datetime.now().hour == 4:
-        project = hopsworks.login(api_key_value= os.environ['HOPSWORKS_API_KEY'], project="project0")
-        fs = project.get_feature_store()
-        klines = client.klines("BTCUSDT", "1d",limit = 1)
-        df = pd.DataFrame(klines, columns =["Open time","Open","High","Low",
-                                            "Close","Volume","Close time",
-                                            "Quote asset volume","Number of trades",
-                                            "Taker buy base asset volume",
-                                            "Taker buy quote asset volume",
-                                            "Ignore"])
-        
-        df['close_time'] =  df['close_time'].div(1000)
-        df['close_time'] =  df['close_time'].div(1000)
-        df = df.astype(float)
-        df["Open time"] = pd.to_datetime(df["Open time"], unit="s")
-        df["Close time"] = pd.to_datetime(df["Close time"], unit="s")
-        df = df.drop('Ignore', axis=1)
-
-        df.columns = df.columns.str.lower().str.replace(' ', '_')
-
-        btc_fg = fs.get_feature_group(name="btc",version=1)
-        btc_fg.insert(df)
     return "200"
